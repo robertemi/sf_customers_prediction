7bd70e130e2c7e45f2d18c5ec6fce28fc9bcaa1a
OOP <roeminovici@gmail.com>
2025-04-12 14:28:44 +0300

    misc

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
             return jsonify({'error': f'Missing columns: {missing_columns}'}), 400
 
         # Extract the Name column and prepare the input features
-        name = df['Name'].iloc[0]
+        name = df['Name'].values[0]
         X = df.drop(columns=['Name'])
 
         # Transform the data using the saved ColumnTransformer
